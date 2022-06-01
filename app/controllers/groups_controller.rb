441f6c0136b6ad403958a89112472d8cf5776a87
Mphatso Lichapa <lichapau@gmail.com>
2022-06-01 18:02:04 +0200

    fixed group view errors

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -28,7 +28,7 @@ class GroupsController < ApplicationController
 
     respond_to do |format|
       if @group.save
-        format.html { redirect_to user_groups_url(@group.user_id), notice: "Group was successfully created." }
+        format.html { redirect_to groups_url, notice: "Group was successfully created." }
         format.json { render :show, status: :created, location: @group }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -41,7 +41,7 @@ class GroupsController < ApplicationController
   def update
     respond_to do |format|
       if @group.update(group_params)
-        format.html { redirect_to user_groups_url(@group.user_id), notice: "Group was successfully updated." }
+        format.html { redirect_to groups_url, notice: "Group was successfully updated." }
         format.json { render :show, status: :ok, location: @group }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -55,7 +55,7 @@ class GroupsController < ApplicationController
     @group.destroy
 
     respond_to do |format|
-      format.html { redirect_to user_groups_url(@group.user_id), notice: "Group was successfully destroyed." }
+      format.html { redirect_to groups_url, notice: "Group was successfully destroyed." }
       format.json { head :no_content }
     end
   end
