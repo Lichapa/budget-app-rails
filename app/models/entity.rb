a9f5a0c3e3be3be60e3b406ec712af8505e6af5f
Mphatso Lichapa <lichapau@gmail.com>
2022-06-03 14:11:11 +0200

    Added tests for models

--- app/models/entity.rb
+++ app/models/entity.rb
@@ -3,6 +3,7 @@ class Entity < ApplicationRecord
   belongs_to :group, class_name: 'Group'
 
   validates :name, :amount, :group_id, :user_id, presence: true
+  validates :amount, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   def self.total_amount(entity)
     sum = 0
